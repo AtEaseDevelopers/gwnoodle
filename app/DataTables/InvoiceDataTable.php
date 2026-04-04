c6f2bf57d029ed0054bb2c3f1a2b782bd1663b59
Asus\User <hengyanchan1@gmail.com>
2026-04-04 11:55:21 +0800

    update

--- app/DataTables/InvoiceDataTable.php
+++ app/DataTables/InvoiceDataTable.php
@@ -164,7 +164,7 @@ class InvoiceDataTable extends DataTable
                             if(columns[index].title == \'Status\'){
                                 var input = \'<select class="border-0" style="width: 100%;"><option value="1">Completed</option><option value="0">New</option></select>\';
                             }else if(columns[index].title == \'Payment Term\'){
-                                var input = \'<select class="border-0" style="width: 100%;"><option value=""></option><option value="1">Cash</option><option value="2">Credit</option></select>\';
+                                var input = \'<select class="border-0" style="width: 100%;"><option value=""></option><option value="1">Cash</option><option value="2">Credit</option><option value="3">Online Payment</option><option value="4">Touch n Go</option><option value="5">Cheque</option></select>\';
                             }else if(columns[index].title == \'Date\'){
                                 var input = \'<input type="text" id="\'+index+\'Date" onclick="searchDateColumn(this);" placeholder="Search ">\';
                             }else if(columns[index].title == \'Group\'){
